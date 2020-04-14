ea1184ad8c78f3ee8b7274cec8a34fa6d834c721
smreddy <smreddy@sintecmedia.com>
2020-04-14 21:22:47 +0530

    Move Element to the end problem

---
+++ src/main/java/com/practice/algoexpert/MoveElementToEnd.java
@@ -0,0 +1,25 @@
+package com.practice.algoexpert;
+
+import java.util.List;
+
+public class MoveElementToEnd {
+
+    public static List<Integer> moveElementToEnd(List<Integer> array, int n) {
+        int i = 0 , j = array.size()-1;
+        while( i < array.size() && j >= 0 && i < j){
+            while(i < array.size() && array.get(i)!= n){
+                i++;
+            }
+            while(j >= 0 && array.get(j) == n){
+                j--;
+            }
+
+            if(i < array.size() && j >= 0 && i < j){
+                int temp = array.get(i);
+                array.set(i,array.get(j));
+                array.set(j,temp);
+            }
+        }
+        return array;
+    }
+}
---
+++ src/test/java/com/practice/algoexpert/MoveElementToEndTest.java
@@ -0,0 +1,62 @@
+package com.practice.algoexpert;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class MoveElementToEndTest {
+
+    @Test
+    public void test1(){
+        int[] source = {1,2,3,2,5,6,7,2,4,5,6,7,2,2,3,1};
+        List<Integer> sourceList = Arrays.stream(source).boxed().collect(Collectors.toList());
+        sourceList = MoveElementToEnd.moveElementToEnd(sourceList,2);
+        int i = 0 ;
+        for(; i < sourceList.size() && sourceList.get(i) != 2; i++){
+        }
+        for(int j = i ; j < sourceList.size();j++){
+            assertTrue(sourceList.get(j) == 2);
+        }
+    }
+
+    @Test
+    public void test2(){
+        int[] source = {2,2,2,2,2,2,2,2,2,2};
+        List<Integer> sourceList = Arrays.stream(source).boxed().collect(Collectors.toList());
+        sourceList = MoveElementToEnd.moveElementToEnd(sourceList,2);
+        int i = 0 ;
+        for(; i < sourceList.size() && sourceList.get(i) != 2; i++){
+        }
+        for(int j = i ; j < sourceList.size();j++){
+            assertTrue(sourceList.get(j) == 2);
+        }
+    }
+
+    @Test
+    public void test3(){
+        int[] source = {2,2,2,2,1,1,1,1,1,2,2,2};
+        List<Integer> sourceList = Arrays.stream(source).boxed().collect(Collectors.toList());
+        sourceList = MoveElementToEnd.moveElementToEnd(sourceList,2);
+        int i = 0 ;
+        for(; i < sourceList.size() && sourceList.get(i) != 2; i++){
+        }
+        for(int j = i ; j < sourceList.size();j++){
+            assertTrue(sourceList.get(j) == 2);
+        }
+    }
+
+    @Test
+    public void test4(){
+        int[] source = {5,5,7,8,9,4,1,1,1,1,1,0};
+        List<Integer> sourceList = Arrays.stream(source).boxed().collect(Collectors.toList());
+        sourceList = MoveElementToEnd.moveElementToEnd(sourceList,2);
+        int i = 0 ;
+        for(; i < sourceList.size() && sourceList.get(i) != 2; i++){
+        }
+        for(int j = i ; j < sourceList.size();j++){
+            assertTrue(sourceList.get(j) == 2);
+        }
+    }
+}
